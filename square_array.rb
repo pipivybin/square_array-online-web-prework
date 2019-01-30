2dd69efa944a8c6c6e3938bbe885c0a885966c95
Ming Duan <11203duan@gmail.com>
2019-01-30 00:16:37 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,10 @@
+# collection = [1,2,3,4]
+
 def square_array(array)
-  # your code here
+  array_new = []
+  array.each do |number|
+    array_new << number ** 2
+  end
+array_new
 end
+  # your code here
